a183d3e74529aa3f0e9fe9b8a861da8f2ae1f061
Joseph Converse <jpc6459@psu.edu>
2023-02-05 20:26:08 -0500

    Giacobe ViteJs

--- src/App.vue
+++ src/App.vue
@@ -20,3 +20,77 @@ import HelloWorld from './components/HelloWorld.vue'
   margin-top: 60px;
 }
 </style>
+
+.wrapper {
+  width: 400px;
+  border: 4px solid green;
+  display: inline-flex;
+}
+
+.image{
+  width: 100%;
+}
+
+.header {
+  text-align: center;
+  font-wieght: bold;
+  font-size: 2rem;
+}
+
+.header h3,
+ header h4 {
+  margin: 18px;
+}
+
+.details {
+  margin-left: 24px;
+  padding: 10px;
+}
+.details summary {
+  font-size: 25px;
+  font-weight: bold;
+}
+
+.buttons button:focus,
+.buttons button:hover {
+  background-color: rgba(50, 0, 200, .5);
+}
+
+.buttons button:active {
+  background-color: rgba(200, 0, 50, .5);
+}
+
+.buttons {
+  display: block;
+}
+
+button {
+  padding: 15px;
+  font-size: 32px;
+  display: block;
+}
+
+@media only screen and (max-width: 600px) {
+  .wrapper {
+    
+   background-color: pink;
+  }
+}
+
+@media only screen and (max-width: 1200px) {
+  .wrapper {
+    
+   background-color: orange;
+  }
+}
+
+@media only screen and (max-width: 425px) {
+  .wrapper {
+    
+   font-size: 1rem;
+   font-wieght: normal;
+  }
+  .details {
+    display: none;
+  }
+}
